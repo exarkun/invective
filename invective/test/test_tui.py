d593b9c86b0eb49e48c3b01f649e98c1541a5339
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2007-01-14 06:10:17 +0000

    top-level setup code and tests

---
+++ invective/test/test_tui.py
@@ -0,0 +1,62 @@
+
+"""
+Tests for the top-level TUI code.
+"""
+
+from twisted.trial.unittest import TestCase
+from twisted.conch.insults.window import TopWindow, VBox, TextOutput
+from twisted.conch.insults.helper import TerminalBuffer
+
+from invective.widgets import LineInputWidget
+from invective.tui import createChatRootWidget, UserInterface
+
+
+class WidgetLayoutTests(TestCase):
+    """
+    Test that widget objects end up in the right place in a widget hierarchy.
+    """
+    def test_rootCreation(self):
+        """
+        Verify that L{createChatRootWidget} gives back a L{TopWindow} with a
+        L{TextOutput} and a L{LineInputWidget} as children.
+        """
+        def painter():
+            pass
+
+        def controller(line):
+            pass
+
+        root = createChatRootWidget(80, 24, painter, controller)
+        self.failUnless(isinstance(root, TopWindow))
+        self.assertIdentical(root.painter, painter)
+        self.assertEqual(len(root.children), 1)
+        vbox = root.children[0]
+        self.failUnless(isinstance(vbox, VBox))
+        self.assertEqual(len(vbox.children), 2)
+        output, input = vbox.children
+        self.failUnless(isinstance(output, TextOutput))
+        self.failUnless(isinstance(input, LineInputWidget))
+        self.assertIdentical(input._realSubmit, controller)
+
+
+class UserInterfaceTests(TestCase):
+    """
+    Test that the TerminalProtocol in charge of the user's terminal behaves in
+    a bunch of desirable ways.
+    """
+    def setUp(self):
+        self.transport = None
+        self.terminal = TerminalBuffer()
+        self.terminal.makeConnection(self.transport)
+        self.protocol = UserInterface()
+
+
+    def test_initialState(self):
+        """
+        Test that immediately after a connection is established the screen is
+        cleared.
+        """
+        # Scribble on the terminal so we can tell that it gets cleared.
+        self.terminal.write('test bytes')
+        self.protocol.makeConnection(self.terminal)
+        self.failUnless(str(self.terminal).isspace())
---
+++ invective/tui.py
@@ -0,0 +1,51 @@
+# -*- test-case-name: invective.test.test_tui -*-
+
+"""
+Create and arrange widgets to form an IRC client.
+"""
+
+from twisted.internet import reactor
+
+from twisted.conch.insults.insults import TerminalProtocol
+from twisted.conch.insults.window import TopWindow, VBox, TextOutput
+
+from invective.widgets import LineInputWidget
+
+# XXX TODO - Use Glade
+def createChatRootWidget(width, height, painter, controller):
+    root = TopWindow(painter)
+    vbox = VBox()
+    vbox.addChild(TextOutput())
+    vbox.addChild(LineInputWidget(width, controller))
+    root.addChild(vbox)
+    return root
+
+
+
+class UserInterface(TerminalProtocol):
+    """
+    Set up an input area and an output area for a chat client.
+    """
+    width = 80
+    height = 24
+
+    def _painter(self):
+        self.rootWidget.draw(self.width, self.height, self.terminal)
+
+
+    def _controller(self, line):
+        pass
+
+
+    def connectionMade(self):
+        super(UserInterface, self).connectionMade()
+        self.terminal.reset()
+        self.rootWidget = createChatRootWidget(self.width, self.height, self._painter, self._controller)
+
+
+    def connectionLost(self, reason):
+        reactor.stop()
+
+
+    def keystrokeReceived(self, keyID, modifier):
+        self.rootWidget.keystrokeReceived(keyID, modifier)
